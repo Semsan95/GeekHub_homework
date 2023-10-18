7214d3543885540320d1f3eab89c0df155ea3c91
Semsan95 <143294027+Semsan95@users.noreply.github.com>
2023-10-18 22:24:01 +0300

    Update task_6.py

--- HT_02/task_6.py
+++ HT_02/task_6.py
@@ -3,6 +3,7 @@ Write a script to check whether a value from user input is contained in a group 
     e.g. [1, 2, 'u', 'a', 4, True] --> 2 --> True
          [1, 2, 'u', 'a', 4, True] --> 5 --> False
 '''
+
 user_value = input('Input any value: ')
 values_list = [1, 2, 'u', 'a', 4, True]
 print(user_value in map(str, values_list))
