e342d46e47190153f498c46d3621e98a39ac73a4
Semsan95 <143294027+Semsan95@users.noreply.github.com>
2023-10-18 22:23:47 +0300

    Update task_3.py

--- HT_02/task_3.py
+++ HT_02/task_3.py
@@ -2,6 +2,7 @@
 Write a script which accepts a <number> from user
 and print out a sum of the first <number> positive integers.
 '''
+
 number = int(input('Input number: '))
 numbers_list = list(range(1, number + 1))
 print(sum(numbers_list))
