ad22375fb2ffed0d904590abc26c3083e577d3f2
Semsan95 <143294027+Semsan95@users.noreply.github.com>
2023-10-22 00:18:03 +0300

    Update task_3.py

--- HT_03/task_3.py
+++ HT_03/task_3.py
@@ -5,14 +5,6 @@ Write a script to concatenate following dictionaries to create a new one.
     dict_3 = {'AUD': 19.2, 'name': 'Tom'}
 '''
 
-'''dict_1 = {'foo': 'bar', 'bar': 'buz'}
-dict_2 = {'dou': 'jones', 'USD': 36}
-dict_3 = {'AUD': 19.2, 'name': 'Tom'}
-temp_list = (dict_1, dict_2, dict_3)
-new_dict = {}
-[new_dict.update(i) for i in temp_list]
-print(new_dict)'''
-
 dict_1 = {'foo': 'bar', 'bar': 'buz'}
 dict_2 = {'dou': 'jones', 'USD': 36}
 dict_3 = {'AUD': 19.2, 'name': 'Tom'}
