d1d9cdb6dd996901f5d0fdab8d6a1e52d7fbffa1
Semsan95 <143294027+Semsan95@users.noreply.github.com>
2023-10-23 21:37:44 +0300

    Update task_4.py

--- HT_03/task_4.py
+++ HT_03/task_4.py
@@ -1,6 +1,9 @@
 '''
 Write a script that combines three dictionaries by updating the first one.
 '''
+# те ж саме (що і в task_3). Методи типів даних не вчив від слова зовсім.
+# Гугли як робитьсбя апдейт словника іншим словником.
+# Перероблено
 
 dict_1 = {'foo': 'bar', 'bar': 'buz'}
 dict_2 = {'dou': 'jones', 'USD': 36}
