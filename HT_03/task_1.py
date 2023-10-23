eb693cd2a603d10a56046008532d0ba383d1eb91
Semsan95 <143294027+Semsan95@users.noreply.github.com>
2023-10-23 21:36:53 +0300

    Update task_1.py

--- HT_03/task_1.py
+++ HT_03/task_1.py
@@ -4,6 +4,8 @@ The list of tuples can be hardcoded.
 The "replacement" value is entered by user.
 The number of elements in the tuples must be different.
 '''
+# можна позбавитись одного рівня вкладеності за рахунок використання and
+# Перероблено
 
 list_of_tuples = [(True, False), ('Text1', 'Text2', 'Text3'), (1, 2, 3, 4)]
 user_input = input('Input your value: ')
