f4a9ab8d6248836057e45e8ee661253c29f534f4
Semsan95 <143294027+Semsan95@users.noreply.github.com>
2023-10-23 21:37:11 +0300

    Update task_2.py

--- HT_03/task_2.py
+++ HT_03/task_2.py
@@ -2,6 +2,8 @@
 Write a script to remove an empty elements from a list.
 Test list: [(), ('hey'), ('',), ('ma', 'ke', 'my'), [''], {}, ['d', 'a', 'y'], '', []]
 '''
+# якось дуже складно вийшло. Робиться без len, range і тим більше reverse
+# Перероблено
 
 test_list = [(), ('hey'), ('',), ('ma', 'ke', 'my'), [''], {}, ['d', 'a', 'y'], '', []]
 for i in reversed(range(len(test_list))):
