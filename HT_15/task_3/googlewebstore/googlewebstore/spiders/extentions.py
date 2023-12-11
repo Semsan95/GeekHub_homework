601530a0913eed3be667e7384844b23eaa709b95
Semsan95 <143294027+Semsan95@users.noreply.github.com>
2023-12-11 12:12:50 +0200

    Update extentions.py

--- HT_15/task_3/googlewebstore/googlewebstore/spiders/extentions.py
+++ HT_15/task_3/googlewebstore/googlewebstore/spiders/extentions.py
@@ -1,3 +1,10 @@
+'''
+Використовуючи Scrapy, заходите на "https://chrome.google.com/webstore/sitemap", 
+переходите на кожен лінк з тегів <loc>, з кожного лінка берете посилання на сторінки екстеншенів, 
+парсите їх і зберігаєте в CSV файл ID, назву та короткий опис кожного екстеншена 
+(пошукайте уважно де його можна взяти)
+'''
+
 import scrapy
 from scrapy import Request
 from bs4 import BeautifulSoup
